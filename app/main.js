da17333ec22b06edf8974a62c13bf70965ad4963
Aleksandr <56314279+AleksPika@users.noreply.github.com>
2021-01-25 21:26:24 +0200

    js scroll

--- app/main.js
+++ app/main.js
@@ -24,4 +24,21 @@ $(function(){
     }
     /* header fixed */
 
+    /*Smooth scrol*/
+    $("[data-scroll]").on("click", function(event){
+        event.preventDefault();
+
+        let $this = $(this),
+            blockId = $this.data('scroll'),
+            blockOffset = $(blockId).offset().top;
+
+        $("#nav a").removeClass("active");
+        $this.addClass("active");
+
+        $("html, body").animate({
+            scrollTop: blockOffset
+        }, 500);
+    });
+    /*Smooth scrol*/
+
 });
